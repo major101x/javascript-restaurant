ffce6444d58bbfedb03a2903b2cf72d70a07f008
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2024-11-22 22:28:02 +0100

    update menuItem to accept argument to append to from menuModule

--- src/menuItem.js
+++ src/menuItem.js
@@ -1,15 +1,24 @@
-const menuItem = ({ name, description, price, image }) => {
+const menuItem = (divToAppendTo, { name, description, price, image }) => {
+  // Select div#content inside template.html
+  const contentDiv = document.querySelector("#content");
+
   // Create elements
   const nameDiv = document.createElement("h3");
   const descriptionDiv = document.createElement("p");
   const priceDiv = document.createElement("p");
-  const image = document.createElement("img");
+  const foodImg = document.createElement("img");
 
   // Add parameter as content to created elements
   nameDiv.textContent = name;
   descriptionDiv.textContent = description;
   priceDiv.textContent = "$" + price;
-  image.src = image;
+  foodImg.src = image;
+
+  // Elements are appended to div passed as argument
+  divToAppendTo.appendChild(foodImg);
+  divToAppendTo.appendChild(nameDiv);
+  divToAppendTo.appendChild(descriptionDiv);
+  divToAppendTo.appendChild(priceDiv);
 };
 
 export default menuItem;
