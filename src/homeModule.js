a51b8129e49dfc91da96e2e3fa69eaf84484fd2c
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2024-11-23 00:26:03 +0100

    change background image

--- src/homeModule.js
+++ src/homeModule.js
@@ -1,4 +1,3 @@
-import foodImage from "./images/food-image.jpg";
 const homeModule = () => {
   // Select div#content inside template.html
   const contentDiv = document.querySelector("#content");
@@ -7,22 +6,16 @@ const homeModule = () => {
   contentDiv.textContent = "";
 
   // Create elements
-  const img = document.createElement("img");
   const h1 = document.createElement("h1");
   const h2 = document.createElement("h2");
 
   // Add content to elements
-  img.src = foodImage;
   h1.textContent = "Welcome to Food For Real!";
   h2.textContent =
     "Being one of the best establishments in God knows where, Food For Real makes quality burgers and provides high-quality customer service for its esteemed customers. Your satisfaction is our top priority, for real!";
 
-  // Append elements to contentDiv
-  contentDiv.appendChild(img);
   contentDiv.appendChild(h1);
   contentDiv.appendChild(h2);
-
-  console.log("Hi");
 };
 
 export default homeModule;
