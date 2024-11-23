85df8b74037dac30b44fcaa0b19834efb99dcf0b
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2024-11-23 01:50:03 +0100

    modify event listeners to add add active-tab class to active tab

--- src/index.js
+++ src/index.js
@@ -7,8 +7,23 @@ const menuBtn = document.querySelector(".menu-btn");
 const contactBtn = document.querySelector(".contact-btn");
 
 // Switch tabs with buttons
-homeBtn.addEventListener("click", homeModule);
-menuBtn.addEventListener("click", menuModule);
-contactBtn.addEventListener("click", contactModule);
+homeBtn.addEventListener("click", () => {
+  homeBtn.classList.add("active-tab");
+  menuBtn.classList.remove("active-tab");
+  contactBtn.classList.remove("active-tab");
+  homeModule();
+});
+menuBtn.addEventListener("click", () => {
+  homeBtn.classList.remove("active-tab");
+  menuBtn.classList.add("active-tab");
+  contactBtn.classList.remove("active-tab");
+  menuModule();
+});
+contactBtn.addEventListener("click", () => {
+  homeBtn.classList.remove("active-tab");
+  menuBtn.classList.remove("active-tab");
+  contactBtn.classList.add("active-tab");
+  contactModule();
+});
 
 homeModule();
