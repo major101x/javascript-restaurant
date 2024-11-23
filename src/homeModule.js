714f98998d34be1f5c9e105e14d83e9995bc1368
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2024-11-23 01:33:12 +0100

    update homeModule

--- src/homeModule.js
+++ src/homeModule.js
@@ -5,6 +5,10 @@ const homeModule = () => {
   // Clear div#content
   contentDiv.textContent = "";
 
+  // Creates wrapper for homeModule
+  const homeDiv = document.createElement("div");
+  contentDiv.appendChild(homeDiv);
+
   // Create elements
   const h1 = document.createElement("h1");
   const h2 = document.createElement("h2");
@@ -14,8 +18,9 @@ const homeModule = () => {
   h2.textContent =
     "Being one of the best establishments in God knows where, Food For Real makes quality burgers and provides high-quality customer service for its esteemed customers. Your satisfaction is our top priority, for real!";
 
-  contentDiv.appendChild(h1);
-  contentDiv.appendChild(h2);
+  // Append elements
+  homeDiv.appendChild(h1);
+  homeDiv.appendChild(h2);
 };
 
 export default homeModule;
