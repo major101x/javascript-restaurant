019ee5454c0e0dfaf450e37b9ac78e8f2b298383
Olayiwola Aroyeun <72233326+major101x@users.noreply.github.com>
2024-11-23 01:29:25 +0100

    update contactModule

--- src/contactModule.js
+++ src/contactModule.js
@@ -4,6 +4,41 @@ const contactModule = () => {
 
   // Clear div#content
   contentDiv.textContent = "";
+
+  // Creates wrapper for contactModule
+  const contactDiv = document.createElement("div");
+  contentDiv.appendChild(contactDiv);
+
+  // Creates hours for opening/closing of restaurant
+  function createHours(day, time) {
+    const hourP = document.createElement("p");
+    const daySpan = document.createElement("span");
+    const timeSpan = document.createElement("span");
+
+    daySpan.textContent = day + ": ";
+    timeSpan.textContent = time;
+
+    hourP.appendChild(daySpan);
+    hourP.appendChild(timeSpan);
+    hoursDiv.appendChild(hourP);
+  }
+
+  // Create elements
+  const phoneNumberDiv = document.createElement("div");
+  const addressDiv = document.createElement("div");
+  const hoursDiv = document.createElement("div");
+
+  // Add content to elements
+  phoneNumberDiv.textContent = "📞 123-456-789";
+  addressDiv.textContent = "🏠 60, Godknowswhere, CBO, Antartica";
+
+  contactDiv.appendChild(phoneNumberDiv);
+  contactDiv.appendChild(addressDiv);
+  contactDiv.appendChild(hoursDiv);
+
+  // call createHours to append hours to hoursDiv
+  createHours("Mon - Fri", "8:00am - 6:00pm");
+  createHours("Sat & Sun", "10:00am - 10:00pm");
 };
 
 export default contactModule;
